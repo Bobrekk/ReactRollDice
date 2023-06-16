e4299dd810e211c73b77e25b3bdd478797beb5da
Bobrekk <81027034+Bobrekk@users.noreply.github.com>
2023-06-16 23:49:56 +0300

    Add files via upload

---
+++ rolldice/src/App.js
@@ -0,0 +1,12 @@
+import RollDice from './components/RollDice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
